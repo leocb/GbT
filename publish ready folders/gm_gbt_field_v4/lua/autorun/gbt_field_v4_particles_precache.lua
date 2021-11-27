4206f121d3e6b3d8bce5270937029e0c3ea44ef4
leocb <leobottaro@gmail.com>
2021-11-27 00:14:46 -0300

    Created "publish ready" version of the main maps without propertary content from valve (pls don't sue me)

---
+++ publish ready folders/gm_gbt_field_v4/lua/autorun/gbt_field_v4_particles_precache.lua
@@ -0,0 +1,7 @@
+game.AddParticles("particles/gbtdevroom_v4_fix.pcf")
+game.AddParticles("particles/gbtdevroom2_v4_fix.pcf")
+
+
+PrecacheParticleSystem("gbtdevsurface")
+PrecacheParticleSystem("gbtdevbolhas")
+PrecacheParticleSystem("freezer")
